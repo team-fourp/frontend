e2f241beb49d31faabfaf4d52a99bdeaf9d6d146
team-fourp <app.fourp.web@gmail.com>
2021-07-09 01:29:44 -0500

    feat: header app missing input search in laptop

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,6 @@
 <template>
   <header class="flex items-center content-center py-3 px-2 relative">
+    <!-- Icon of menu -->
     <button class="lg:hidden w-8 h-8" @click="isShowMenu = true">
       <!-- <img src="@/static/ui/menu.svg" alt="Menu icon" > -->
       <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -10,19 +11,18 @@
       </svg>
     </button>
     <Logotype class="ml-2" />
-    <nav class="ml-auto flex flex-wrap max-h-8 items-center content-center invisible lg:visible">
-      <ul class="flex flex-wrap items-center content-center">
-        <li v-for="item in items" :key="item.iconName" class="mr-2">
-          <nuxt-link :to="item.href">{{ item.title }}</nuxt-link>
-        </li>
-      </ul>
-    </nav>
-    <div class="flex items-center content-center relative">
+
+    <!-- Icon of left (mobile) -->
+
+    <div class="ml-auto flex items-center content-center lg:hidden">
       <label for="search-header" class="cursor-pointer w-8 h-8 flex items-center justify-center">
         <img src="@/static/ui/search.svg" alt="Search icon">
       </label>
+      <button v-if="false" class="lg:hidden w-8 h-8 ml-2" @click="isShowMenu = true">
+        <img src="@/static/ui/menu.svg" alt="Menu icon">
+      </button>
       <div class="main-search">
-        <div class="bg-white flex items-center opacity-0 w-0 z-10 absolute inset-0 h-full py-3 px-2 search-input">
+        <div class="bg-white flex items-center opacity-0 w-0 z-10 absolute top-0 left-0 h-full py-3 px-2 search-input">
           <button class="cursor-pointer h-full w-8 mr-2" @click="focusSearch">
             <img
             src="@/static/ui/search.svg"
@@ -38,10 +38,33 @@
           <img src="@/static/ui/x.svg" alt="Close icon" class="cursor-pointer ml-1">
         </div>
       </div>
-      <button v-if="false" class="lg:hidden w-8 h-8 ml-2" @click="isShowMenu = true">
-        <img src="@/static/ui/menu.svg" alt="Menu icon">
+    </div>
+
+    <!-- Input search (laptop)  -->
+    <div class="ml-auto">
+
+    </div>
+
+    <!-- Navigation (laptop)-->
+    <nav class="mr-4 flex flex-wrap max-h-8 items-center content-center hidden lg:block">
+      <ul class="flex flex-wrap items-center content-center">
+        <li v-for="item in items" :key="item.iconName" class="mr-2">
+          <nuxt-link :to="item.href">{{ item.title }}</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+
+    <!-- Buttons signin / signup (laptop) -->
+    <div class="hidden lg:block">
+      <button class="px-3 max-w-lg rounded bg-blue-600 py-1">
+        <span class="text-sm font-bolder text-white">Inicia sesión</span>
+      </button>
+      <button class="px-3 max-w-lg rounded bg-red-600 py-1">
+        <span class="text-sm font-bolder text-white">Comenzar aventura</span>
       </button>
     </div>
+
+    <!-- Lateral menu -->
     <div
       class="fixed top-0 right-0 h-screen z-10 w-full transition-transform transform delay-100 duration-300"
       :class="{ 'translate-x-0': isShowMenu, '-translate-x-full': !isShowMenu }">
@@ -49,6 +72,7 @@
       :class="{ 'aside-menu-visible': isShowMenu, 'aside-menu-invisible': !isShowMenu }">
       </div>
       <div class="relative w-100 mr-auto sm:w-10/12 md:w-2/5 bg-white h-screen py-3">
+        <!-- Header lateral menu -->
         <div class="flex items-center px-2">
           <img src="@/static/logos/icon.svg" alt="FourP logo" width="32" class="sm:hidden">
           <span class="font-bold ml-1 text-sm sm:text-base">Menú</span>
@@ -56,6 +80,7 @@
             <img src="@/static/ui/x.svg" alt="Icon close menu">
           </button>
         </div>
+        <!-- Links lateral menu -->
         <div class="pt-4">
           <div
             v-for="item in items"
@@ -74,6 +99,8 @@
             </nuxt-link>
           </div>
         </div>
+
+        <!-- Buttons lateral menu -->
         <div class="w-full bottom-0 absolute left-0 flex flex-col items-center px-2 pb-4">
           <button class="w-full max-w-lg rounded bg-blue-600 py-1 mb-2">
             <span class="text-sm font-bolder text-white">Inicia sesión</span>
